bdef6942c528fbcca97ec472a99947c9ff0de656
MAUS502 <manngt@gmail.com>
2022-01-25 15:05:26 -0600

    Create exportAccountProperties.ps1
    New script to obtain account properties using a CSV containing data of a field and export it to CSV

---
+++ exportAccountProperties.ps1
@@ -0,0 +1,25 @@
+ #@dev502
+
+#Script to obtain account properties and save it into csv file
+Import-Module ActiveDirectory
+
+#Create a csv file with data and use it to search account porperties into the Active Directory, generally samAccountName is common to use
+$path = 'path/to/your/csv/file';
+
+#Save de data into an array variable
+$members = Import-Csv C:\samaacount.csv -Header "samAccountNameHeader"
+
+#Create an array to save te results
+$admembers = @()
+
+
+#Loop each data item saved into members and add it to admembers with se properties you like
+foreach($member in $members){
+  $admember = $member.CODIGO
+  $admembers += Get-ADUser -Filter{Initials -eq $admember} -Properties * | Select Initials, EmailAddress,samAccountName
+}
+
+#Save the result into a csv to use
+$admembers | Export-Csv -Path "path/to/save/csv/file/results" -NoTypeInformation
+
+#Name the variables as you pleased 
